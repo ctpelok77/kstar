52487a908709d284c9c7ad37c70b34c0b119af0f
Dominik Winterer <dominik.winterer@ibm.com>
2017-07-26 10:43:46 -0400

    interim

--- src/search/search_engines/kstar.h
+++ src/search/search_engines/kstar.h
@@ -17,21 +17,24 @@ protected:
 	bool first_plan_found;
 	int optimal_solution_cost;
 	std::priority_queue<std::pair<int, s_StateActionPair>> queue_djkstra;
-	PerStateInformation<bool> heap_initialized;
+	std::unordered_set<StateID> heap_initialized;
     std::map<s_StateActionPair, s_StateActionPair> parent_sap;
 	std::map<s_StateActionPair, bool> cross_edge;
+    std::set<long int> closed;
 
 	void djkstra_search();
-
 	vector<s_StateActionPair> djkstra_traceback(std::pair<int, s_StateActionPair>& top_pair);
 	vector<s_StateActionPair> compute_sidetrack_seq(std::pair<int, s_StateActionPair>& top_pair,
 													vector<s_StateActionPair>& path);
+	bool is_initialized(GlobalState &s);
+	void set_initialized(GlobalState &s);
+	void reset_initialized();
 	void add_plan(std::pair<int, s_StateActionPair>& p);
 	void add_goal_heap_top();
 	void add_cross_edges(std::pair<int, s_StateActionPair> p);
 	void add_first_plan();
-	void dump_astar_search_space();
 	std::string get_node_name(std::pair<int, s_StateActionPair>& p);
+	bool enough_plans_found();
 	void notify_generate(std::pair<int, s_StateActionPair>& p);
 	void notify_push(std::pair<int, s_StateActionPair>& p);
 	void notify_expand(std::pair<int, s_StateActionPair>& p);
