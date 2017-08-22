0a0bfde93df5298541e2437e30bc5988ace57235
Michael-Katz1 <michael.katz1@ibm.com>
2017-08-22 15:00:51 -0400

    modified parameter

--- src/search/kstar/kstar.cc
+++ src/search/kstar/kstar.cc
@@ -189,18 +189,26 @@ void add_simple_plans_only_option(OptionParser &parser) {
 
  static SearchEngine *_parse(OptionParser &parser) {
 	 parser.add_option<ScalarEvaluator *>("eval", "evaluator for h-value");
-	 top_k_eager_search::add_top_k_option(parser);
+	 parser.add_option<int>("plans", "Number of plans", "5");
+
+//	 top_k_eager_search::add_top_k_option(parser);
 	 top_k_eager_search::add_pruning_option(parser);
 	 add_simple_plans_only_option(parser);
 	 SearchEngine::add_options_to_parser(parser);
 	 Options opts = parser.parse();
-	 KStar *engine = nullptr; if (!parser.dry_run()) {
+	 KStar *engine = nullptr;
+	 if (!parser.dry_run()) {
+		 int num_plans = opts.get<int>("plans");
+		 cout << "Running K* with K=" <<num_plans << endl;
 		 auto temp = search_common::create_astar_open_list_factory_and_f_eval(opts);
 		 opts.set("open", temp.first);
 		 opts.set("f_eval", temp.second);
 		 opts.set("reopen_closed", true);
 		 std::vector<Heuristic *> preferred_list;
 		 opts.set("preferred", preferred_list);
+
+		 opts.set("K", num_plans);
+
 		 engine = new KStar(opts);
 	 }
 
