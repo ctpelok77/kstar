037066b6cdcb9baa69777942a75d45410ab8d912
Dominik Winterer <dominik.winterer@ibm.com>
2017-08-25 10:05:15 -0400

    interim

--- src/search/kstar/plan_reconstructor.h
+++ src/search/kstar/plan_reconstructor.h
@@ -21,6 +21,7 @@ public:
 	std::vector<Node> compute_sidetrack_seq(std::vector<Node>& path);
 	void extract_plan(vector<Node>& seq, Plan &plan, StateSequence &state_seq);
 	bool is_simple_plan(StateSequence seq, StateRegistry* state_registry);
+	void inc_optimal_plans(Plan &plan);
 	void set_goal_state(StateID goal_state);
 	void add_plan(Node node, std::vector<Plan>& top_k_plans, bool simple_plans_only);
 };
