10cbb833cb7094a2d82b3676660141384533a0d6
Dominik Winterer <dominik.winterer@ibm.com>
2017-08-25 09:10:36 -0400

    several changes

--- src/search/search_statistics.cc
+++ src/search/search_statistics.cc
@@ -19,6 +19,10 @@ SearchStatistics::SearchStatistics() {
     dead_end_states = 0;
     generated_ops = 0;
 
+	num_plans_found = 0;	
+	num_djkstra_runs = 0;	
+	total_djkstra_node_generations = 0;
+
     lastjump_expanded_states = 0;
     lastjump_reopened_states = 0;
     lastjump_evaluated_states = 0;
@@ -88,4 +92,9 @@ void SearchStatistics::print_detailed_statistics() const {
         cout << "Improving symmetrical states until last jump: "
              << last_jump_improving_symmetrical_states << endl;
     }
+
+	cout << "Number of plans found: "<< num_plans_found << std::endl;
+	cout << "Number of djkstra runs: "<< num_djkstra_runs << std::endl;
+	cout << "Total number of djkstra node generations: " 
+			  << total_djkstra_node_generations << std::endl;
 }
