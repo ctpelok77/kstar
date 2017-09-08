98c4322ab0f2866b17b5e47c6863bc4b3eea287c
Dominik Winterer <dominik.winterer@ibm.com>
2017-09-08 14:34:59 -0400

    reducing the output of the planner for experiments

--- src/search/kstar/kstar.cc
+++ src/search/kstar/kstar.cc
@@ -189,7 +189,7 @@ bool KStar::djkstra_search() {
 			return false;
 		queue_djkstra.pop();
 
-		notify_expand(node, &state_registry, num_node_expansions);
+		//notify_expand(node, &state_registry, num_node_expansions);
         cout << "" << endl;
 		plan_reconstructor->add_plan(node, top_k_plans, simple_plans_only);
 		statistics.inc_plans_found();
