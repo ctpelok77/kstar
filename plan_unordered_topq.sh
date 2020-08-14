dc641ce93671ed1316f190f78ba873c58572c10a
Michael Katz <michael.katz1@ibm.com>
2020-08-14 00:55:04 -0400

    added shell script

---
+++ plan_unordered_topq.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# $1 domain
+# $2 problem
+# $3 quality bound (q)
+# $4 upper bound on the number of plans (k)
+# $5 json file name
+
+if [ "$#" -ne 5 ]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
+
+RUNOPT="kstar(blind(),q=$3,k=$4,skip_reorderings=true,json_file_to_dump=$5)"
+
+LOG_FILE=run.log
+
+SOURCE="$( dirname "${BASH_SOURCE[0]}" )"
+#echo $SOURCE
+$SOURCE/fast-downward.py --build release64 $1 $2 --search $RUNOPT > $LOG_FILE
+
